2e82f3d62ca1dcc4e659904c0d8b4e6273a83674
Manuel Montenegro <mmontene@ucm.es>
2017-12-26 21:11:23 +0100

    Removed equals and hashCode()

--- src/main/kotlin/es/ucm/caviart/Lexer.kt
+++ src/main/kotlin/es/ucm/caviart/Lexer.kt
@@ -1,61 +1,136 @@
+/*
+    Copyright (c) 2018 Manuel Montenegro
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 package es.ucm.caviart
 
 import java.io.Reader
 
+/*
+ * The following constants represent special characters
+ * in CLIR source code files.
+ */
+private const val LEFT_PAREN = '('.toInt()
+private const val RIGHT_PAREN = ')'.toInt()
+private const val QUOTE = '"'.toInt()
+private const val LF = '\n'.toInt()
+private const val CR = '\r'.toInt()
+private const val UTF8_BOM = 0xFEFF
+
+
+/**
+ * Instances of this class represent a token of a CLIR file.
+ *
+ * @property line Line number of this token (starting from 1)
+ * @property column Column number of this token (starting from 1)
+ */
 abstract class Token(val line: Int, val column: Int)
 
+/**
+ * Token specifying a left parenthesis symbol.
+ *
+ * @param line Line number of this token (starting from 1)
+ * @param column Column number of this token (starting from 1)
+ */
 class LeftParen(line: Int, column: Int) : Token(line, column) {
-    override fun equals(other: Any?): Boolean {
-        return other is LeftParen && other.line == line && other.column == column
-    }
-
+    /**
+     * String representation of the token
+     */
     override fun toString(): String {
         return "LeftParen($line, $column)"
     }
+
 }
 
+/**
+ * Token specifying a right parenthesis symbol.
+ *
+ * @param line Line number of this token (starting from 1)
+ * @param column Column number of this token (starting from 1)
+ */
 class RightParen(line: Int, column: Int) : Token(line, column) {
-    override fun equals(other: Any?): Boolean {
-        return other is RightParen && other.line == line && other.column == column
-    }
-
+    /**
+     * String representation of the token
+     */
     override fun toString(): String {
         return "RightParen($line, $column)"
     }
 }
 
+/**
+ * Token specifying a literal symbol (a sequence of characters different from parenthesis or quotes)
+ *
+ * @param line Line number of this token (starting from 1)
+ * @param column Column number of this token (starting from 1)
+ */
 class TokenLiteral(line: Int, column: Int, val value: String) : Token(line, column) {
-    override fun equals(other: Any?): Boolean {
-        return other is TokenLiteral && other.line == line && other.column == column && other.value == value
-    }
-
+    /**
+     * String representation of the token
+     */
     override fun toString(): String {
         return "TokenLiteral($line, $column, \"$value\")"
     }
 }
 
-val LEFT_PAREN = '('.toInt()
-val RIGHT_PAREN = ')'.toInt()
-val QUOTE = '"'.toInt()
-val LF = '\n'.toInt()
-val CR = '\r'.toInt()
-val UTF8_BOM = 0xFEFF
-
+/**
+ * It returns the sequence of tokens appearing in the given input stream.
+ *
+ * @param reader Input stream to examine
+ * @return List of [Token] instances.
+ */
 fun readTokens(reader: Reader): List<Token> {
     val result = mutableListOf<Token>()
 
+    // Last character read
     var currentChar = reader.read()
+
+    // Current word (sequence of characters separated by spaces)
     var currentWord = StringBuffer()
+
+    // If we are inside a quoted string
     var betweenQuotes = false
+
+    // Line number of the last character read
     var line = 1
+
+    // Column number of the last character read
     var column = 1
+
+    // Column number in which the current token starts
     var beginningColumnToken = 1
+
+    // Whether the previous character was a CR symbol
     var lastCR = false
 
+    // If the first character of the file is a BOM mark, we simply
+    // discard it
     if (currentChar == UTF8_BOM) currentChar = reader.read()
+
+
 
     while (currentChar != -1) {
         when {
+
+            // If we are inside a quoted string, we add the current character to the current word
+            // or finish the word, if the character is a quote mark
+
             currentChar != QUOTE && betweenQuotes ->
                 currentWord.appendCodePoint(currentChar)
 
@@ -64,6 +139,9 @@ fun readTokens(reader: Reader): List<Token> {
                 currentWord = StringBuffer()
                 betweenQuotes = false
             }
+
+
+            // Starting quoted string, if we are not within a word.
 
             currentChar == QUOTE && !betweenQuotes -> {
                 if (currentWord.isEmpty()) {
@@ -74,12 +152,16 @@ fun readTokens(reader: Reader): List<Token> {
                 }
             }
 
+            // A whitespace character finishes the current word
+
             Character.isWhitespace(currentChar) -> {
                 if (!currentWord.isEmpty()) {
                     result.add(TokenLiteral(line, beginningColumnToken, currentWord.toString()))
                     currentWord = StringBuffer()
                 }
             }
+
+            // A left or right parenthesis finishes the current word and adds the corresponding token
 
             currentChar == LEFT_PAREN -> {
                 if (!currentWord.isEmpty()) {
@@ -97,6 +179,8 @@ fun readTokens(reader: Reader): List<Token> {
                 result.add(RightParen(line, column))
             }
 
+            // Otherwise, we add the character to the current word
+
             else -> {
                 if (currentWord.isEmpty()) {
                     beginningColumnToken = column
@@ -105,6 +189,7 @@ fun readTokens(reader: Reader): List<Token> {
             }
         }
 
+        // We advance the line and column counters if necessary
         when {
             currentChar == CR -> {
                 line++
@@ -134,4 +219,4 @@ fun readTokens(reader: Reader): List<Token> {
     return result
 }
 
-class UnexpectedQuoteException(val line: Int, val column: Int): Exception("Unexpected quote symbol: line $line, column $column")
+class UnexpectedQuoteException(val line: Int, val column: Int) : Exception("Unexpected quote symbol: line $line, column $column")
--- src/test/kotlin/es/ucm/caviart/LexerTest.kt
+++ src/test/kotlin/es/ucm/caviart/LexerTest.kt
@@ -7,6 +7,11 @@ import kotlin.test.assertTrue
 import kotlin.test.fail
 
 class LexerTest {
+
+    private fun assertEqualsToken(t1: Token, t2: Token) {
+        assertEquals(t1.toString(), t2.toString())
+    }
+
     @Test fun emptyString() {
         val tokens = readTokens(StringReader(""))
 
@@ -17,65 +22,65 @@ class LexerTest {
         val tokens = readTokens(StringReader("token"))
 
         assertEquals(1, tokens.size, "Single token")
-        assertEquals(TokenLiteral(1, 1, "token"), tokens[0])
+        assertEqualsToken(TokenLiteral(1, 1, "token"), tokens[0])
     }
 
     @Test fun twoTokens() {
         val tokens = readTokens(StringReader("val 3"))
 
         assertEquals(2, tokens.size, "Two tokens")
-        assertEquals(TokenLiteral(1, 1, "val"), tokens[0])
-        assertEquals(TokenLiteral(1, 5, "3"), tokens[1])
+        assertEqualsToken(TokenLiteral(1, 1, "val"), tokens[0])
+        assertEqualsToken(TokenLiteral(1, 5, "3"), tokens[1])
     }
 
     @Test fun threeTokensNewLine() {
         val tokens = readTokens(StringReader("val 45\n 54"))
 
         assertEquals(3, tokens.size, "Three tokens")
-        assertEquals(TokenLiteral(1, 1, "val"), tokens[0])
-        assertEquals(TokenLiteral(1, 5, "45"), tokens[1])
-        assertEquals(TokenLiteral(2, 2, "54"), tokens[2])
+        assertEqualsToken(TokenLiteral(1, 1, "val"), tokens[0])
+        assertEqualsToken(TokenLiteral(1, 5, "45"), tokens[1])
+        assertEqualsToken(TokenLiteral(2, 2, "54"), tokens[2])
     }
 
     @Test fun parens() {
         val tokens = readTokens(StringReader("f((d\n) ("))
         assertEquals(6, tokens.size, "Six tokens")
-        assertEquals(TokenLiteral(1, 1, "f"), tokens[0])
-        assertEquals(LeftParen(1, 2), tokens[1])
-        assertEquals(LeftParen(1, 3), tokens[2])
-        assertEquals(TokenLiteral(1, 4, "d"), tokens[3])
-        assertEquals(RightParen(2, 1), tokens[4])
-        assertEquals(LeftParen(2, 3), tokens[5])
+        assertEqualsToken(TokenLiteral(1, 1, "f"), tokens[0])
+        assertEqualsToken(LeftParen(1, 2), tokens[1])
+        assertEqualsToken(LeftParen(1, 3), tokens[2])
+        assertEqualsToken(TokenLiteral(1, 4, "d"), tokens[3])
+        assertEqualsToken(RightParen(2, 1), tokens[4])
+        assertEqualsToken(LeftParen(2, 3), tokens[5])
     }
 
     @Test fun parensWithSpaces() {
         val tokens = readTokens(StringReader("f ( ( \n d \n ) ("))
         assertEquals(6, tokens.size, "Six tokens")
-        assertEquals(TokenLiteral(1, 1, "f"), tokens[0])
-        assertEquals(LeftParen(1, 3), tokens[1])
-        assertEquals(LeftParen(1, 5), tokens[2])
-        assertEquals(TokenLiteral(2, 2, "d"), tokens[3])
-        assertEquals(RightParen(3, 2), tokens[4])
-        assertEquals(LeftParen(3, 4), tokens[5])
+        assertEqualsToken(TokenLiteral(1, 1, "f"), tokens[0])
+        assertEqualsToken(LeftParen(1, 3), tokens[1])
+        assertEqualsToken(LeftParen(1, 5), tokens[2])
+        assertEqualsToken(TokenLiteral(2, 2, "d"), tokens[3])
+        assertEqualsToken(RightParen(3, 2), tokens[4])
+        assertEqualsToken(LeftParen(3, 4), tokens[5])
     }
 
     @Test fun onlyParens() {
         val tokens = readTokens(StringReader(" (( ) ( )"))
         assertEquals(5, tokens.size, "Five tokens")
-        assertEquals(LeftParen(1, 2), tokens[0])
-        assertEquals(LeftParen(1, 3), tokens[1])
-        assertEquals(RightParen(1, 5), tokens[2])
-        assertEquals(LeftParen(1, 7), tokens[3])
-        assertEquals(RightParen(1, 9), tokens[4])
+        assertEqualsToken(LeftParen(1, 2), tokens[0])
+        assertEqualsToken(LeftParen(1, 3), tokens[1])
+        assertEqualsToken(RightParen(1, 5), tokens[2])
+        assertEqualsToken(LeftParen(1, 7), tokens[3])
+        assertEqualsToken(RightParen(1, 9), tokens[4])
     }
 
     @Test fun Quotes() {
         val tokens = readTokens(StringReader("(\"hello other\" guy)"))
         assertEquals(4, tokens.size, "Four tokens")
-        assertEquals(LeftParen(1, 1), tokens[0])
-        assertEquals(TokenLiteral(1, 2, "hello other"), tokens[1])
-        assertEquals(TokenLiteral(1, 16, "guy"), tokens[2])
-        assertEquals(RightParen(1, 19), tokens[3])
+        assertEqualsToken(LeftParen(1, 1), tokens[0])
+        assertEqualsToken(TokenLiteral(1, 2, "hello other"), tokens[1])
+        assertEqualsToken(TokenLiteral(1, 16, "guy"), tokens[2])
+        assertEqualsToken(RightParen(1, 19), tokens[3])
     }
 
     @Test fun QuotesAfterToken() {
