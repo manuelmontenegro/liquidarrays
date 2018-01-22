812f4e8cfef6c44b5bd09bbf035e3d8db29029a5
Manuel Montenegro <mmontene@ucm.es>
2018-01-22 16:40:03 +0100

    Monomorphize initial environment

--- src/main/kotlin/es/ucm/caviart/InitialEnvironment.kt
+++ src/main/kotlin/es/ucm/caviart/InitialEnvironment.kt
@@ -1,16 +1,19 @@
 package es.ucm.caviart
 
+import es.ucm.caviart.ast.*
+import es.ucm.caviart.typecheck.GlobalEnvironment
+
 
 private val logicFunctions = mapOf(
         "+" to UninterpretedFunctionType(listOf(ConstrType("int"), ConstrType("int")), ConstrType("int")),
         "-" to UninterpretedFunctionType(listOf(ConstrType("int"), ConstrType("int")), ConstrType("int")),
         "*" to UninterpretedFunctionType(listOf(ConstrType("int"), ConstrType("int")), ConstrType("int")),
         "/" to UninterpretedFunctionType(listOf(ConstrType("int"), ConstrType("int")), ConstrType("int")),
-        "get-array" to UninterpretedFunctionType(listOf(ConstrType("array", listOf(VarType("'a"))),
-                ConstrType("int")), VarType("'a")),
-        "set-array" to UninterpretedFunctionType(listOf(ConstrType("array", listOf(VarType("'a"))),
-                ConstrType("int"), VarType("'a")), ConstrType("array", listOf(VarType("'a")))),
-        "len" to UninterpretedFunctionType(listOf(ConstrType("array", listOf(VarType("'a")))), ConstrType("int"))
+        "get-array" to UninterpretedFunctionType(listOf(ConstrType("array", listOf(ConstrType("int"))),
+                ConstrType("int")), ConstrType("int")),
+        "set-array" to UninterpretedFunctionType(listOf(ConstrType("array", listOf(ConstrType("int"))),
+                ConstrType("int"), ConstrType("int")), ConstrType("array", listOf(ConstrType("int")))),
+        "len" to UninterpretedFunctionType(listOf(ConstrType("array", listOf(ConstrType("int")))), ConstrType("int"))
 )
 
 private val logicPredicates = mapOf(
@@ -18,7 +21,7 @@ private val logicPredicates = mapOf(
         ">" to listOf(ConstrType("int"), ConstrType("int")),
         "<=" to listOf(ConstrType("int"), ConstrType("int")),
         ">=" to listOf(ConstrType("int"), ConstrType("int")),
-        "=" to listOf(VarType("'a"), VarType("'a"))
+        "=" to listOf(ConstrType("int"), ConstrType("int"))
 )
 
 val initialEnvironment = GlobalEnvironment(
@@ -35,11 +38,11 @@ val initialEnvironment = GlobalEnvironment(
                                     FunctionApplication(name, argNames.map { Variable(it) }))))))
                     )
                 } + logicPredicates.map { (name, types) ->
-                            val argNames = types.mapIndexed { index, _ -> "x_$index" }
-                            name to FunctionalType(
-                                    argNames.zip(types).map { (name, hmType) -> TypedVar(name, QualType("nu", hmType, True())) },
-                                    listOf(TypedVar("res", QualType("nu", ConstrType("bool"),
-                                            BooleanEquality(BooleanVariable("nu"), PredicateApplication(name, argNames.map { Variable(it) })))))
-                            )
-                        }).toMap().toMutableMap()
+                    val argNames = types.mapIndexed { index, _ -> "x_$index" }
+                    name to FunctionalType(
+                            argNames.zip(types).map { (name, hmType) -> TypedVar(name, QualType("nu", hmType, True())) },
+                            listOf(TypedVar("res", QualType("nu", ConstrType("bool"),
+                                    Iff(BooleanVariable("nu"), PredicateApplication(name, argNames.map { Variable(it) })))))
+                    )
+                }).toMap().toMutableMap()
 )
