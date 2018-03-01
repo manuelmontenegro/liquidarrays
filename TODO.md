9485b2e71197dc6979f6fd9cf470568231ae3d70
Manuel Montenegro <mmontene@ucm.es>
2018-03-01 17:52:10 +0100

    Cleanup

--- TODO.md
+++
@@ -1,6 +0,0 @@
-
-## TODOs
-
-* Define tuple declarations for each tuple found in the whole program.
-* Constructors must be defined beforehand in the IR.
-
--- src/main/kotlin/es/ucm/caviart/LiquidEnvironment.kt
+++
@@ -1,9 +0,0 @@
-package es.ucm.caviart
-
-
-abstract class EnvironmentEntry(val next: EnvironmentEntry?)
-
-class AssertionEntry(val assertion: Assertion, next: EnvironmentEntry?): EnvironmentEntry(next)
-
-class VariableEntry(val variable: String, val type: QualType, next: EnvironmentEntry?): EnvironmentEntry(next)
-
